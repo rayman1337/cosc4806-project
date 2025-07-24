d41339f5b8e0c72af27cfa1f5b387e81171c185a
RM <134272733+rayman1337@users.noreply.github.com>
2025-07-24 18:26:39 +0000

    Implemented saveMovie in Movies Model

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -7,5 +7,9 @@ class Movie {
         return $stmt->fetch();
     }
 
-  
+    public static function saveMovie($data) {
+        global $pdo;
+        $stmt = $pdo->prepare("INSERT INTO movies (title, year, genre, imdb_id, metascore, imdb_rating, description) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        return $stmt->execute([$data['title'], $data['year'], $data['genre'], $data['imdb_id'], $data['metascore'], $data['imdb_rating'], $data['description']]);
+    }
 }
